04992fa36cec52741eddcae7ace321fab6b147ca
MarcosStingher <marcos_antoniostingher@yahoo.com.br>
2024-06-25 19:48:29 -0300

    Segundo commit

--- syban_pnx/priv/repo/migrations/20240610220441_create_dispositivo.exs
+++ syban_pnx/priv/repo/migrations/20240610220441_create_dispositivo.exs
@@ -3,7 +3,7 @@ defmodule SybanPnx.Repo.Migrations.CreateDispositivo do
 
   def change do
     create table(:dispositivo, primary_key: false) do
-      add :idmaquina, :bigserial, primary_key: true
+      add :id, :bigserial, primary_key: true
       add :nomedispositivo, :string
 
       timestamps(type: :utc_datetime)
