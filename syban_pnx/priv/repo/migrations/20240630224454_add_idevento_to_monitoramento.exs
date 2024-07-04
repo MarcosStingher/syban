beecda3cf9a567f66aba59e92b1f58c8d40e8c62
MarcosStingher <marcos_antoniostingher@yahoo.com.br>
2024-07-03 23:31:37 -0300

    excluir migration

--- syban_pnx/priv/repo/migrations/20240630224454_add_idevento_to_monitoramento.exs
+++
@@ -1,9 +0,0 @@
-defmodule SybanPnx.Repo.Migrations.AddIdeventoToMonitoramento do
-  use Ecto.Migration
-
-  def change do
-    alter table(:monitoramento) do
-      add :idevento, :integer, default: 1, null: false
-    end
-  end
-end
